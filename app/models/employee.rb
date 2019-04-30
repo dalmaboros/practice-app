189d20bccf2e663857317e1ac0da4c3d4c5450e0
Dalma Boros <dalma.boros@gmail.com>
2019-04-30 13:46:21 -0500

    Add model associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  has_many :jobs
+  has_many :shops, :through => :jobs
+  
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,5 @@
 class Shop < ApplicationRecord
+  has_many :jobs
+  has_many :employees, :through => :jobs
+  
 end
