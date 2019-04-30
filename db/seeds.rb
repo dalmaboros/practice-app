f01587b8ed9fdd8ebaea65af68be1d399e120a31
Dalma Boros <dalma.boros@gmail.com>
2019-04-30 13:51:52 -0500

    Create, migrate, and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shop.create(name:"Philistine", city: "Toronto")
+Shop.create(name:"DNO", city: "New Orleans")
+
+Employee.create(name:"Aaron", shop_ids:[1])
+Employee.create(name:"Jill", shop_ids:[1,2])
